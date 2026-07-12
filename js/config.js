5040f1c4d7a24692c330b347344c1e94f6c0edb2
ismaelriosorellana-hash <ismaelrios.orellana@gmail.com>
2026-07-12 07:09:01 -0400

    Frontend v5.8.1 - Restaurar carga de catalogo y fichas

--- js/config.js
+++ js/config.js
@@ -28,7 +28,7 @@ const PLACEHOLDER_IMAGE = "data:image/svg+xml;charset=UTF-8," + encodeURICompone
 </svg>`);
 
 window.CONFIG = Object.freeze({
-  APP_VERSION: "5.8.0-ficha-producto-clara",
+  APP_VERSION: "5.8.1-hotfix-carga-productos",
   SITE_NAME: "Rhema Diseños",
   BRAND_NAME: "Rhema Diseños",
   SITE_URL: "https://emmagina-frontend.onrender.com",
